d0e2f6635148d65ff2c79114c44a39e124aec038
zjspj <43582909+zjspj@users.noreply.github.com>
2019-05-12 18:42:49 -0700

    Update busco.sh

--- analysis/busco.sh
+++ analysis/busco.sh
@@ -12,15 +12,10 @@ conda activate final_project_1
 
 
 USERNAME='jiadony1'
-INPUT_DATA_TYPE='pilon'
+INPUT_DATA_TYPE='HGAP'
 INPUT_FASTA=7_pilon/consensus_second_pilon.fasta
 
-
-export AUGUSTUS_CONFIG_PATH=/data/users/${USERNAME}/bin/miniconda3/pkgs/augustus-3.2.2-0/config
-
-export PATH=$PATH:/data/users/${USERNAME}/bin/miniconda3/pkgs/augustus-3.2.2-0/scripts
-
-busco -c ${NSLOTS} -i /pub/${USERNAME}/canu_job/${INPUT_FASTA} -l /pub/${USERNAME}/rawdata/busco/saccharomycetales_odb9 -o /pub/${USERNAME}/canu_job/9_busco_${INPUT_DATA_TYPE} -m geno -sp saccharomyces_cerevisiae_S288C
+busco -c ${NSLOTS} -i /pub/${USERNAME}/canu_job/${INPUT_FASTA} -l /pub/${USERNAME}/rawdata/busco/saccharomycetales_odb9 -o 9_busco_${INPUT_DATA_TYPE} -m geno -sp saccharomyces_cerevisiae_S288C
 
 
 conda deactivate
