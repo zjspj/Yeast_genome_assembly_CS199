e41f44dacdf5067defbad89e146da8256f61a32e
Renhao Luo <37392460+RenhaoL@users.noreply.github.com>
2019-05-25 10:00:54 -0700

    Update busco.sh

--- analysis/busco.sh
+++ analysis/busco.sh
@@ -7,15 +7,13 @@
 #$ -ckpt restart
 #$ -pe openmp 8
 
+USER_NAME='jiadony1'
+INPUT_DATA_TYPE='HGAP'
+INPUT_FASTA=7_pilon/consensus_pilon.fasta # change the file name to different outputs from different steps.
+
 source ~/.miniconda3rc
 conda activate final_project_1
 
-
-USERNAME='jiadony1'
-INPUT_DATA_TYPE='HGAP'
-INPUT_FASTA=7_pilon/consensus_second_pilon.fasta
-
-busco -c ${NSLOTS} -i /pub/${USERNAME}/canu_job/${INPUT_FASTA} -l /pub/${USERNAME}/rawdata/busco/saccharomycetales_odb9 -o 9_busco_${INPUT_DATA_TYPE} -m geno -sp saccharomyces_cerevisiae_S288C
-
+busco -c ${NSLOTS} -i /pub/${USER_NAME}/canu_job/${INPUT_FASTA} -l /pub/${USER_NAME}/rawdata/busco/saccharomycetales_odb9 -o 9_busco_${INPUT_DATA_TYPE} -m geno -sp saccharomyces_cerevisiae_S288C
 
 conda deactivate
