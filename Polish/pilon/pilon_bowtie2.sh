526f4010674b9ee7bb875d676a528c358d16f5b4
Renhao Luo <37392460+RenhaoL@users.noreply.github.com>
2019-05-25 09:55:55 -0700

    Update pilon_bowtie2.sh

--- Polish/pilon/pilon_bowtie2.sh
+++ Polish/pilon/pilon_bowtie2.sh
@@ -8,15 +8,16 @@
 #$ -ckpt restart
 #$ -pe openmp 8
 
-source ~/.miniconda3rc
-conda activate final_project_1
-
+USER_NAME=jiadony1
 SAMPLE_NAME="SRR1569900"
 SAMPLE_1="${SAMPLE_NAME}_1.fastq"
 SAMPLE_2="${SAMPLE_NAME}_2.fastq"
-WORK_DIR=/pub/jiadony1/canu_job/7_pilon
+WORK_DIR=/pub/${USER_NAME}/canu_job/7_pilon
 SAMPLE_REFERENCE="consensus"
-ILLUMINA_SAMPLE_NAME="S288C"
+ILLUMINA_SAMPLE_NAME="W303"
+
+source ~/.miniconda3rc
+conda activate final_project_1
 
 bowtie2 --threads ${NSLOTS} -x ${WORK_DIR}/${SAMPLE_REFERENCE} -1 ${WORK_DIR}/${SAMPLE_1} -2 ${WORK_DIR}/${SAMPLE_2} -S ${WORK_DIR}/${ILLUMINA_SAMPLE_NAME}_${SAMPLE_REFERENCE}.sam
 
