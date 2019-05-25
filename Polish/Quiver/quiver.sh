87872f80956179814ea1d95f8131dd543744ccf0
Renhao Luo <37392460+RenhaoL@users.noreply.github.com>
2019-05-25 09:47:55 -0700

    Update quiver.sh

--- Polish/Quiver/quiver.sh
+++ Polish/Quiver/quiver.sh
@@ -7,18 +7,18 @@
 #$ -ckpt restart
 #$ -pe openmp 8
 
-source ~/.miniconda3rc
-conda activate final_project_1
-
-QUERY="out_all.cmp.h5"
+QUERY="out_all.cmp.h5" # from previous step. This is a file combined all PacBio raw data.
 REFERENCE="5_canu.contigs.fasta" # from canu
 SECOND_REF="consensus.fasta" # from the first round of quiver
 
+source ~/.miniconda3rc
+conda activate final_project_1
 
+#first round of quiver. 
 samtools faidx ${REFERENCE}
 quiver ${QUERY} -r ${REFERENCE} -o variants.gff -o consensus.fasta -o consensus.fastq
 
-
+#second round of quiver. 
 samtools faidx ${SECOND_REF}
 quiver ${QUERY} -r ${SECOND_REF} -o variants_second.gff -o consensus_second.fasta -o consensus_second.fastq
 
