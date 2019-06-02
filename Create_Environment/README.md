37871ede5d0f96ab741245111b013fa005348578
Mike <43582909+zjspj@users.noreply.github.com>
2019-06-01 17:24:00 -0700

    new

--- Create_Environment/README.md
+++ Create_Environment/README.md
@@ -1,1 +1,2 @@
 
+# This folder sets up the folder needed for the pipeline and downloads the appropriate file to appropriate location.
---
+++ Create_Environment/download_data.sh
@@ -0,0 +1,63 @@
+#!/bin/bash!
+
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0001/Analysis_Results/m130605_000141_42207_c100515142550000001823076608221372_s1_p0.1.bax.h5 -O ./rawdata/pacbio/0001/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0001/Analysis_Results/m130605_000141_42207_c100515142550000001823076608221372_s1_p0.2.bax.h5 -O ./rawdata/pacbio/0001/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0001/Analysis_Results/m130605_000141_42207_c100515142550000001823076608221372_s1_p0.3.bax.h5 -O ./rawdata/pacbio/0001/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0001/Analysis_Results/m130605_000141_42207_c100515142550000001823076608221372_s1_p0.bas.h5 -O ./rawdata/pacbio/0001/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0001/m130605_000141_42207_c100515142550000001823076608221372_s1_p0.metadata.xml -O ./rawdata/pacbio/0001
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0002/Analysis_Results/m130605_032054_42207_c100515142550000001823076608221373_s1_p0.1.bax.h5 -O ./rawdata/pacbio/0002/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0002/Analysis_Results/m130605_032054_42207_c100515142550000001823076608221373_s1_p0.2.bax.h5 -O ./rawdata/pacbio/0002/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0002/Analysis_Results/m130605_032054_42207_c100515142550000001823076608221373_s1_p0.3.bax.h5 -O ./rawdata/pacbio/0002/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0002/Analysis_Results/m130605_032054_42207_c100515142550000001823076608221373_s1_p0.bas.h5 -O ./rawdata/pacbio/0002/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0002/m130605_032054_42207_c100515142550000001823076608221373_s1_p0.metadata.xml -O ./rawdata/pacbio/0002
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0007/Analysis_Results/m130605_231954_42210_c100515112550000001823076608221304_s1_p0.1.bax.h5 -O ./rawdata/pacbio/0007/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0007/Analysis_Results/m130605_231954_42210_c100515112550000001823076608221304_s1_p0.2.bax.h5 -O ./rawdata/pacbio/0007/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0007/Analysis_Results/m130605_231954_42210_c100515112550000001823076608221304_s1_p0.3.bax.h5 -O ./rawdata/pacbio/0007/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0007/Analysis_Results/m130605_231954_42210_c100515112550000001823076608221304_s1_p0.bas.h5 -O ./rawdata/pacbio/0007/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0007/m130605_231954_42210_c100515112550000001823076608221304_s1_p0.metadata.xml -O ./rawdata/pacbio/0007
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0011/Analysis_Results/m130607_031915_42207_c100539492550000001823089611241310_s1_p0.1.bax.h5 -O ./rawdata/pacbio/0011/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0011/Analysis_Results/m130607_031915_42207_c100539492550000001823089611241310_s1_p0.2.bax.h5 -O ./rawdata/pacbio/0011/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0011/Analysis_Results/m130607_031915_42207_c100539492550000001823089611241310_s1_p0.3.bax.h5 -O ./rawdata/pacbio/0011/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0011/Analysis_Results/m130607_031915_42207_c100539492550000001823089611241310_s1_p0.bas.h5 -O ./rawdata/pacbio/0011/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0011/m130607_031915_42207_c100539492550000001823089611241310_s1_p0.metadata.xml -O ./rawdata/pacbio/0011
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0012/Analysis_Results/m130607_063829_42207_c100539492550000001823089611241311_s1_p0.1.bax.h5 -O ./rawdata/pacbio/0012/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0012/Analysis_Results/m130607_063829_42207_c100539492550000001823089611241311_s1_p0.2.bax.h5 -O ./rawdata/pacbio/0012/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0012/Analysis_Results/m130607_063829_42207_c100539492550000001823089611241311_s1_p0.3.bax.h5 -O ./rawdata/pacbio/0012/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0012/Analysis_Results/m130607_063829_42207_c100539492550000001823089611241311_s1_p0.bas.h5 -O ./rawdata/pacbio/00012/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0012/m130607_063829_42207_c100539492550000001823089611241311_s1_p0.metadata.xml -O ./rawdata/pacbio/0012
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0013/Analysis_Results/m130607_131654_42207_c100539492550000001823089611241313_s1_p0.1.bax.h5 -O ./rawdata/pacbio/0013/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0013/Analysis_Results/m130607_131654_42207_c100539492550000001823089611241313_s1_p0.2.bax.h5 -O ./rawdata/pacbio/0013/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0013/Analysis_Results/m130607_131654_42207_c100539492550000001823089611241313_s1_p0.3.bax.h5 -O ./rawdata/pacbio/0013/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0013/Analysis_Results/m130607_131654_42207_c100539492550000001823089611241313_s1_p0.bas.h5 -O ./rawdata/pacbio/0013/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0013/m130607_131654_42207_c100539492550000001823089611241313_s1_p0.metadata.xml -O ./rawdata/pacbio/0013
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0014/Analysis_Results/m130607_095747_42207_c100539492550000001823089611241312_s1_p0.1.bax.h5 -O ./rawdata/pacbio/0014/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0014/Analysis_Results/m130607_095747_42207_c100539492550000001823089611241312_s1_p0.2.bax.h5 -O ./rawdata/pacbio/0014/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0014/Analysis_Results/m130607_095747_42207_c100539492550000001823089611241312_s1_p0.3.bax.h5 -O ./rawdata/pacbio/0014/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0014/Analysis_Results/m130607_095747_42207_c100539492550000001823089611241312_s1_p0.bas.h5 -O ./rawdata/pacbio/0014/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0014/m130607_095747_42207_c100539492550000001823089611241312_s1_p0.metadata.xml -O ./rawdata/pacbio/0014
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0018/Analysis_Results/m130609_051143_42207_c100539282550000001823089611241380_s1_p0.1.bax.h5 -O ./rawdata/pacbio/0018/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0018/Analysis_Results/m130609_051143_42207_c100539282550000001823089611241380_s1_p0.2.bax.h5 -O ./rawdata/pacbio/0018/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0018/Analysis_Results/m130609_051143_42207_c100539282550000001823089611241380_s1_p0.3.bax.h5 -O ./rawdata/pacbio/0018/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0018/Analysis_Results/m130609_051143_42207_c100539282550000001823089611241380_s1_p0.bas.h5 -O ./rawdata/pacbio/0018/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0018/m130609_051143_42207_c100539282550000001823089611241380_s1_p0.metadata.xml -O ./rawdata/pacbio/0018
+wget https://sra-download.ncbi.nlm.nih.gov/traces/sra24/SRZ/001284/SRR1284669/SRR1284669_hdf5.tgz -O ./rawdata/pacbio/0019/Analysis_Results
+tar -zxvf ./rawdata/pacbio/0019/Analysis_Results/SRR1284669_hdf5.tgz -C ./rawdata/pacbio/0019/Analysis_Results
+rm ./rawdata/pacbio/0019/Analysis_Results/SRR1284669_hdf5.tgz
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0019/m130609_050958_42210_c100539382550000001823089611241350_s1_p0.metadata.xml -O ./rawdata/pacbio/0019
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0020/Analysis_Results/m130609_083056_42207_c100539282550000001823089611241381_s1_p0.1.bax.h5 -O ./rawdata/pacbio/0020/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0020/Analysis_Results/m130609_083056_42207_c100539282550000001823089611241381_s1_p0.2.bax.h5 -O ./rawdata/pacbio/0020/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0020/Analysis_Results/m130609_083056_42207_c100539282550000001823089611241381_s1_p0.3.bax.h5 -O ./rawdata/pacbio/0020/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0020/Analysis_Results/m130609_083056_42207_c100539282550000001823089611241381_s1_p0.bas.h5 -O ./rawdata/pacbio/0020/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0020/m130609_083056_42207_c100539282550000001823089611241381_s1_p0.metadata.xml -O ./rawdata/pacbio/0020
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0021/Analysis_Results/m130609_082855_42210_c100539382550000001823089611241351_s1_p0.1.bax.h5 -O ./rawdata/pacbio/0021/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0021/Analysis_Results/m130609_082855_42210_c100539382550000001823089611241351_s1_p0.2.bax.h5 -O ./rawdata/pacbio/0021/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0021/Analysis_Results/m130609_082855_42210_c100539382550000001823089611241351_s1_p0.3.bax.h5 -O ./rawdata/pacbio/0021/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0021/Analysis_Results/m130609_082855_42210_c100539382550000001823089611241351_s1_p0.bas.h5 -O ./rawdata/pacbio/0021/Analysis_Results
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/0021/m130609_082855_42210_c100539382550000001823089611241351_s1_p0.metadata.xml -O ./rawdata/pacbio/0021
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR156/000/SRR1569900/SRR1569900_1.fastq.gz -O ./rawdata/illumina
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR156/000/SRR1569900/SRR1569900_2.fastq.gz -O ./rawdata/illumina
+
+wget http://datasets.pacb.com.s3.amazonaws.com/2013/Yeast/HGAP_Assembly/polished_assembly.fasta -O ./rawdata/REF/HGAP_assembly.fasta
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/292/815/GCA_000292815.1_ASM29281v1/GCA_000292815.1_ASM29281v1_genomic.fna.gz -O ./rawdata/REF
+gunzip ./rawdata/REF/GCA_000292815.1_ASM29281v1_genomic.fna.gz > ./rawdata/REF/illumina_MPG_2013.fasta
