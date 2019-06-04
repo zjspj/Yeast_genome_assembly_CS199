e6be4f254e26a5563dacdc4b979e65f6d19380c0
zjspj <43582909+zjspj@users.noreply.github.com>
2019-06-03 22:15:43 -0700

    Update busco.sh

--- Analysis/busco/busco.sh
+++ Analysis/busco/busco.sh
@@ -7,6 +7,10 @@
 #$ -ckpt restart
 #$ -pe openmp 8
 
+
+
+
+
 INPUT_DATA_TYPE='Final_Assembly'
 INPUT_FASTA=../7_pilon/consensus_pilon.fasta 
 
@@ -18,6 +22,9 @@ INPUT_FASTA=../7_pilon/consensus_pilon.fasta
 
 source ~/.miniconda3rc
 conda activate final_project_1
+
+export AUGUSTUS_CONFIG_PATH=/data/users/jiadony1/bin/miniconda3/pkgs/augustus-3.2.2-0/config
+export PATH=$PATH:/data/users/jiadony1/bin/miniconda3/pkgs/augustus-3.2.2-0/scripts
 
 busco -c ${NSLOTS} -i ${INPUT_FASTA} -l ../../rawdata/busco/saccharomycetales_odb9 -o 9_busco_${INPUT_DATA_TYPE} -m geno -sp saccharomyces_cerevisiae_S288C
 
